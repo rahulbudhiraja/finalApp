44c653f43e9c3757cb4aa07528b61c0fdad9c3f7
Rahul Budhiraja <rahul.budhiraja.dark@gmail.com>
2013-08-20 07:48:13 +0300

    Adding the UI

--- src/com/tesseract/studio3d/replace/LoaderImageView.java
+++ src/com/tesseract/studio3d/replace/LoaderImageView.java
@@ -1,8 +1,10 @@
 package com.tesseract.studio3d.replace;
 
 import android.content.Context;
-import android.util.AttributeSet;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.view.Gravity;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
@@ -19,10 +21,29 @@ public class LoaderImageView extends RelativeLayout {
 
    private void instantiate(final Context _context) {
        context = _context;
+       
        imageView = new ImageView(context);
+       
+       int w = 960/2, h = 540/2;
+
+       Bitmap.Config conf = Bitmap.Config.ARGB_8888; // see other conf types
+       Bitmap bmp = Bitmap.createBitmap(w, h, conf); // this creates a MUTABLE bitmap
+       imageView.setImageBitmap(bmp);
+       
        progressBar = new ProgressBar(context);
        progressBar.setIndeterminate(true);
+       progressBar.getIndeterminateDrawable().setColorFilter(0xFFFFFFFF,android.graphics.PorterDuff.Mode.MULTIPLY);
+     
+       RelativeLayout.LayoutParams rlayoutParams = new RelativeLayout.LayoutParams(
+	    		RelativeLayout.LayoutParams.WRAP_CONTENT,
+	    		RelativeLayout.LayoutParams.WRAP_CONTENT);
+   
 
+       rlayoutParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
+       rlayoutParams.addRule(RelativeLayout.CENTER_VERTICAL);
+      
+       progressBar.setLayoutParams(rlayoutParams);
+       
        addView(progressBar);
        addView(imageView);
 
@@ -34,7 +55,14 @@ public class LoaderImageView extends RelativeLayout {
    // Then, play with this method to show or hide your progressBar
    public void toggleVisibilty(boolean imgVisibility,boolean barVisibility) {
     
-    instantiate(context);
+    
+   }
+   
+   public void setImage(Bitmap img)
+   {
+	   imageView.setImageBitmap(img);
+	   progressBar.setVisibility(View.INVISIBLE);
+	   
    }
 
 }
--- src/com/tesseract/studio3d/replace/ReplaceActivity.java
+++ src/com/tesseract/studio3d/replace/ReplaceActivity.java
@@ -1,29 +1,32 @@
 package com.tesseract.studio3d.replace;
 
-import org.opencv.android.OpenCVLoader;
-import org.opencv.core.Mat;
+import java.io.IOException;
+import java.io.InputStream;
 
-import com.tesseract.studio3d.R;
-import com.tesseract.studio3d.Animation.PhotoActivity;
-import com.tesseract.studio3d.R.string;
-import com.tesseract.studio3d.utils.Structs;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
-import android.graphics.Color;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
-import android.view.View;
-import android.view.View.OnClickListener;
+import android.view.ViewGroup.LayoutParams;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
-import android.widget.Toast;
+import android.widget.ScrollView;
+
+import com.tesseract.studio3d.R;
+import com.tesseract.studio3d.utils.Structs;
 
 public class ReplaceActivity extends Activity
 {
@@ -32,9 +35,12 @@ public class ReplaceActivity extends Activity
 	private String selectedImagePath;
 	public Mat foreground,background;
 	public Mat finalImage;
-	RelativeLayout mainLayout;
+	RelativeLayout tempLayout;
+	LinearLayout parentLayout;
 	LoaderImageView customView;
-	 
+	int startingIndex=111222,totalPlaces;
+	ScrollView mainScrollView;
+	String[] placesArray;
 	
 	protected void onCreate(Bundle savedInstanceState) 
 	{
@@ -45,39 +51,47 @@ public class ReplaceActivity extends Activity
 		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		
+	    tempLayout=new RelativeLayout(this);
+	    tempLayout.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+	    
+	    parentLayout=new LinearLayout(this);
+	    parentLayout.setOrientation(LinearLayout.VERTICAL);
+	    
+		mainScrollView=new ScrollView(this);
+		mainScrollView.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+		mainScrollView.addView(parentLayout);
 		
-	    customView=new LoaderImageView(this);
-	   
-	    mainLayout=new RelativeLayout(this);
-	    //mainLayout.setBackgroundColor(Color.BLACK);
-	    
-	    RelativeLayout.LayoutParams llayoutParams = new RelativeLayout.LayoutParams(
-	    		RelativeLayout.LayoutParams.WRAP_CONTENT,
-	    		RelativeLayout.LayoutParams.WRAP_CONTENT);
-	    //mainLayout=(RelativeLayout)findViewById(R.id.replacelayout);
-	    mainLayout.setLayoutParams(llayoutParams);
-	    mainLayout.addView(customView);
-	    
-	   // replaceButton=(Button)findViewById(R.id.browsetoReplace);
-//	    replaceButton.setOnClickListener(new OnClickListener(){
-//
-//			@Override
-//			public void onClick(View v) {
-//				// TODO Auto-generated method stub
-//				 
-//				Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-//			    intent.setType("image/*");
-//			    startActivityForResult(intent, 1337);
-//			  
-//			}
-//	    	
-//	    });
-	    
-	   
-	    setContentView(mainLayout);
+		addPlacesCards();
+		 
+	    setContentView(mainScrollView);
 		
 	    initializeMats();
+	    
+	    backgroundThread.start();
+	   
 	}
+	
+	private boolean listAssetFiles(String path) {
+
+	    String [] list;
+	    try {
+	        list = getAssets().list(path);
+	        if (list.length > 0) {
+	            // This is a folder
+	            for (String file : list) {
+	                if (!listAssetFiles(path + "/" + file))
+	                    return false;
+	            }
+	        } else {
+	            // This is a file
+	            // TODO: add file name to an array list
+	    }
+	        } catch (IOException e) {
+	        return false;
+	    }
+
+	    return true; 
+	} 
 	
 	public  void initializeMats() {
 		// TODO Auto-generated method stub
@@ -101,7 +115,7 @@ public class ReplaceActivity extends Activity
              
              Log.d("ReplaceActivity","Path: "+selectedImagePath);
              
-             getThreshold(Structs.mRgba.getNativeObjAddr(), Structs.mDisparity.getNativeObjAddr(), finalImage.getNativeObjAddr(), background.getNativeObjAddr(),foreground.getNativeObjAddr(),0,0,6,selectedImagePath);
+//             getThreshold(Structs.mRgba.getNativeObjAddr(), Structs.mDisparity.getNativeObjAddr(), finalImage.getNativeObjAddr(), background.getNativeObjAddr(),foreground.getNativeObjAddr(),0,0,6,selectedImagePath);
              
 	    }
 	    
@@ -127,6 +141,113 @@ public class ReplaceActivity extends Activity
 		    
 	}
 	
-	public native void getThreshold(long matAddrRgba, long matAddrDisp, long matAddrfinalImage,long matAddrBackground, long matAddrForeground, int ji1, int ji2,int choice,String imgPath);
+	public void addPlacesCards()
+	{
+		/// Open the Places folder in the image directory.
+		totalPlaces=0;
+		RelativeLayout.LayoutParams customLayoutParams;
+		
+		try {
+			totalPlaces=getAssets().list("images/Places").length;
+			Log.d("List","number: "+getAssets().list("images/Places").length);
+			
+			placesArray=getAssets().list("images/Places");
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		/// Count the number of files and create that many image views ...
+		for(int i=0;i<totalPlaces/2;i++)
+		{
+			
+			Log.d("File names","name"+placesArray[i]);
+			
+		    tempLayout=new RelativeLayout(this);
+		    tempLayout.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
+			
+			
+			  customView=new LoaderImageView(this);
+			  customView.setBackgroundResource(R.drawable.border);
+			  customView.setId(startingIndex+i);
+			  
+			  customLayoutParams= new RelativeLayout.LayoutParams(
+			    		RelativeLayout.LayoutParams.WRAP_CONTENT,
+			    		RelativeLayout.LayoutParams.WRAP_CONTENT);
+			  
+			  customView.setLayoutParams(customLayoutParams);
+			  tempLayout.addView(customView);
+
+			  customView=new LoaderImageView(this);
+			  customView.setBackgroundResource(R.drawable.border);
+			  customView.setId(startingIndex+i+1);
+			  
+			  customLayoutParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+			  
+			  tempLayout.addView(customView);
+			  tempLayout.invalidate();
+			  parentLayout.addView(tempLayout);
+			  
+		}
+		
+		// Start loading the modified images in a thread and keep updating the layout ..
+		
+	}	
+		
+	Thread backgroundThread =new Thread()
+	{
+		
+	 public void run()
+	 {
+		 for (int i=0;i<totalPlaces;i++)
+		 {
+			 try {
+				InputStream is=getAssets().open("images/Places/"+placesArray[i]);
+				// Get bitmap...
+				
+				Bitmap Img=BitmapFactory.decodeStream(is);
+				
+				Mat imgToLoad=new Mat();
+				Utils.bitmapToMat(Img, imgToLoad);
+				
+				
+				Log.d("ReplaceActivity","val:  "+imgToLoad.rows()+"  "+imgToLoad.cols());
+				// Take this mat and pass it to getThresholdFunction
+				getThreshold(Structs.mRgba.getNativeObjAddr(), Structs.mDisparity.getNativeObjAddr(), finalImage.getNativeObjAddr(), background.getNativeObjAddr(),foreground.getNativeObjAddr(),0,0,6,imgToLoad.getNativeObjAddr());
+				
+				// Get recomputed bitmap 
+				
+				Imgproc.cvtColor(imgToLoad, imgToLoad, Imgproc.COLOR_BGR2RGBA);
+	            
+				Bitmap tempBitmap=Bitmap.createBitmap(background.cols(),background.rows(), 
+	            		 Bitmap.Config.ARGB_8888);
+	             
+	            Utils.matToBitmap(background, tempBitmap);
+	             
+	             
+				
+				
+				// Update the UI and assign the bitmap tp cusomview  
+				customView=(LoaderImageView) findViewById(startingIndex+i);
+				
+				customView.setImage(tempBitmap);
+				// invalidate ;;
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			 
+			 
+			 
+		 }
+	 }
+	};
+	
+	
+//	public native void getThreshold(long matAddrRgba, long matAddrDisp, long matAddrfinalImage,long matAddrBackground, long matAddrForeground, int ji1, int ji2,int choice,String imgPath);
+	public native void getThreshold(long matAddrRgba, long matAddrDisp, long matAddrfinalImage,long matAddrBackground, long matAddrForeground, int ji1, int ji2,int choice,long LoadedImageMat);
 	
 }
